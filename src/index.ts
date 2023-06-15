ea07a7402fe688d4ebc09d6f491d45601f42fc3d
Rafortes <rafortes987@gmail.com>
2023-06-15 14:00:01 -0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    await prisma.user.create({
+      data: {
+        name: 'Rafael',
+        email: 'rafael@prisma.io',
+      },
+    })
+
+    const allUsers = await prisma.user.findMany()
+    console.dir(allUsers, { depth: null })
+  }
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
